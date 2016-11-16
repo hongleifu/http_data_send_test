0b28bb853424aa0552c21ad76d1c9ded8f36740a
hongleifu <hongleifu@creditease.cn>
2016-11-16 10:17:55 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # http_data_send_test
+使用说明：
+1.修改data文件中的ip,port,path,data 字段为测试需要发送的内容
+2.执行python http_send_data.py
+  通过查看log打印的"return result"内容看发送数据的返回结果
---
+++ http_send_data.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+#coding=utf8
+import json
+import urllib 
+import urllib2 
+
+def send():
+    #read json string from file
+    fp=open('./data','r')
+    file_content=fp.read()
+    fp.close()
+    file_content = file_content.replace('$','')
+    file_content = file_content.replace('\n','')
+    file_content = file_content.decode('utf8')
+    
+    print "read file content succ!" 
+    content=json.loads(file_content)
+    print "convert to json string succ!" 
+    print content
+
+    #make url
+    ip=content['ip']
+    port=content['port']
+    path=content['path']
+    url=ip+":"+port+path
+    data=content['data'] 
+
+    url=url+urllib.urlencode(data)
+    try:
+        print "send url:-------------\n",url
+        req=urllib2.Request(url)
+        res_data=urllib2.urlopen(req)
+        res=res_data.read()
+        res=res.decode('utf8')
+        print " return result----------------------------------:",res
+    except Exception,e:
+        print "send error:",e
+
+if __name__=="__main__":
+    send()
